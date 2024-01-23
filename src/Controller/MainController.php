97190e09d2478becee62185b49cd54782aa1e24d
EugeniuPeruzzi <jaka1997@live.it>
2024-01-23 14:55:26 +0100

    creazione classi User User profile ono to one e il seguente Create di user e userprofile

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Entity\UserProfile;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\UserProfileRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 // AbstractController è una classe di base fornita da Symfony che a sua volta estende la classe Controller e aggiunge molte funzionalità e servizi utili, tra cui l'iniezione delle dipendenze, l'accesso al servizio del gestore delle richieste (Request), e l'accesso ai metodi di rendering delle viste.
 class MainController extends AbstractController{
@@ -25,15 +29,26 @@ class MainController extends AbstractController{
     ];
 
     // Definizione di una rotta con un parametro opzionale "limit" che deve essere un intero (default: 3)
-    #[Route('/{limit<\d+>?3}', name: 'app_index')]
-    public function index(int $limit): Response {
-        // Restituisce una risposta renderizzata utilizzando il template 'main/index.html.twig'
-        // La variabile 'variabile' contiene una stringa ottenuta unendo gli elementi dell'array $this->messages fino al limite specificato
+    #[Route('/', name: 'app_index')]
+        public function index(UserProfileRepository $profiles, EntityManagerInterface $entityManager): Response {
+
+            $user = new User();
+            $user->setEmail('email@email.com');
+            $user->setPassword('12345678');
+
+            $profile = new UserProfile();
+            $profile->setUser($user);
+            $entityManager->persist($profile);
+            $entityManager->flush();
+
+            // $profile = $profiles->find(1);
+            // $profiles->remove($profile, true);
+            // $entityManager->flush();
         return $this->render(
             'main/index.html.twig',
             [
                 'variabili' => $this->messages,
-                'limit' => $limit
+                'limit' => 3
             ]
         );
     
