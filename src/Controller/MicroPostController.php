72f13de8a680c8ad7431979c15d443897daa91b6
EugeniuPeruzzi <jaka1997@live.it>
2024-01-23 10:28:00 +0100

    gestione form e la sua validazione tramite backend

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -7,6 +7,7 @@ use App\Repository\MicroPostRepository;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -64,20 +65,44 @@ class MicroPostController extends AbstractController
         ]);
     }
 
-
     #[Route('/micro-post/new', name: 'app_micro_post_new', priority: 2)]
-    public function add() : Response {
-
+    public function add(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // Creazione di un nuovo oggetto MicroPost
         $microPost = new MicroPost();
-        
+    
+        // Creazione di un form per gestire l'input dell'utente
         $form = $this->createFormBuilder($microPost)
             ->add('title')
             ->add('text')
             ->add('submit', SubmitType::class, ['label' => 'save'])
             ->getForm();
-
-        return $this->render( 'micro_post/new.html.twig', [
-            'form' => $form
+    
+        // Gestione della richiesta HTTP per il form
+        $form->handleRequest($request);
+    
+        // Verifica se il form è stato inviato e se i dati sono validi
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Ottieni i dati dal form
+            $microPost = $form->getData();
+    
+            // Imposta la data e l'ora corrente
+            $microPost->setDatetime(new DateTime());
+    
+            // Persisti l'oggetto MicroPost nel database
+            $entityManager->persist($microPost);
+            
+            // Esegui la sincronizzazione delle modifiche nel database
+            $entityManager->flush();
+    
+            // Redirect a una pagina successiva alla creazione del MicroPost (aggiungi un URL appropriato)
+            return $this->redirectToRoute('app_micro_post');
+        }
+    
+        // Renderizza la pagina del form
+        return $this->render('micro_post/new.html.twig', [
+            'form' => $form->createView(), // Passa la vista del form al template Twig
         ]);
     }
+    
 }
